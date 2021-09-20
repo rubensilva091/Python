c7e47bb10a37acb4b724922f108b2d59af66793c
Ruben Silva <58857937+rubensilva091@users.noreply.github.com>
2021-09-20 19:27:57 +0100

    self

--- 100DaysPython Course/3.Advanced 59-80/d62 Coffee_Website/main.py
+++ 100DaysPython Course/3.Advanced 59-80/d62 Coffee_Website/main.py
@@ -51,12 +51,12 @@ def add_cafe():
     if form.validate_on_submit():
         with open("cafe-data.csv", mode="a") as csv_file:
             csv_file.write(f"\n{form.cafe.data},"
-                           f"{form.location.data},"
-                           f"{form.open.data},"
-                           f"{form.close.data},"
-                           f"{form.coffee_rating.data},"
-                           f"{form.wifi_rating.data},"
-                           f"{form.power_rating.data}")
+                           f"{form.cafe_location.data},"
+                           f"{form.opening_time.data},"
+                           f"{form.closing_time.data},"
+                           f"{form.coffee_rate.data},"
+                           f"{form.wifi_rate.data},"
+                           f"{form.power_socket_available.data}")
         return redirect(url_for('cafes'))
     return render_template('add.html', form=form)
 
@@ -69,9 +69,6 @@ def cafes():
         for row in csv_data:
             list_of_rows.append(row)
     return render_template('cafes.html', cafes=list_of_rows)
-
-
-
 
 
 if __name__ == '__main__':
