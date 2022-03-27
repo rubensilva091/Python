fcfd149c4b7f251e02734547b0d737f0d56199c1
Ruben Silva <58857937+rubensilva091@users.noreply.github.com>
2022-03-27 17:01:16 +0100

    100%

--- LA2/Treino 2/viagem.py
+++ LA2/Treino 2/viagem.py
@@ -6,7 +6,6 @@ passa o autocarro, intercalada com o custo para viajar entre cada par de cidades
 Assuma que cada rota funciona nos dois sentidos.
 '''
 
-
 rotas1 = [["Porto", 20, "Lisboa"],
           ["Braga", 3, "Barcelos", 4, "Viana", 3, "Caminha"],
           ["Braga", 3, "Famalicao", 3, "Porto"],
@@ -20,8 +19,11 @@ rotas2 = [["Porto", 20, "Lisboa"],
           ["Braga", 3, "Famalicao", 3, "Porto"],
           ["Viana", 4, "Povoa", 3, "Porto"],
           ["Lisboa", 10, "Evora", 8, "Beja", 8, "Faro"],
-          ["Porto", 15, "Lisboa", 20, "Faro"]
+          ["Porto", 15, "Lisboa", 20, "Faro"],
+          ["Zelem", 10, "Persetburgs"]
           ]
+
+rotas3 = []
 
 
 def floyd_warshall(adj):
@@ -43,7 +45,7 @@ def floyd_warshall(adj):
     return dist
 
 
-def viagem(rotas, o, d):
+def viagem1(rotas, o, d):
     adj = {}
     for rota in rotas:
         for i in range(len(rota)-2):
@@ -57,12 +59,46 @@ def viagem(rotas, o, d):
                 adj[rota[i+2]][rota[i]] = rota[i+1]
 
     path = floyd_warshall(adj)
-    if len(path)<1:
+    if len(path) < 1:
         return 0
     return path[o][d]
 
+# ____________________________________________________________________________________
 
-print(viagem(rotas2, "Braga", "Faro"))
 
-# self.assertEqual(viagem(rotas1,"Caminha","Lisboa"),30)
-# self.assertEqual(viagem(rotas2,"Braga","Faro"),41)
+def viagem2(rotas, o, dest):
+    adj = {}
+    pai = {}
+    dist = {}
+    dist[o] = 0
+    orla = {o}
+    while orla:
+        v = min(orla, key=lambda x: dist[x])
+        orla.remove(v)
+
+        if v not in adj:
+            adj[v] = {}
+
+        for rota in rotas:
+            if v in rota:
+                for i in range(len(rota)):
+                    if (rota[i] == v):
+                        if (i-2 >= 0):
+                            adj[v][rota[i-2]] = rota[i-1]
+                        if (i+2 < len(rota)):
+                            adj[v][rota[i+2]] = rota[i+1]
+
+        # Percorrer as edges
+        for d in adj[v]:
+            if d not in dist:
+                orla.add(d)
+                dist[d] = float("inf")
+            if dist[v] + adj[v][d] < dist[d]:
+                pai[d] = v
+                dist[d] = dist[v] + adj[v][d]
+            if d == dest:
+                orla = {}
+                break
+    if dest not in dist:
+        return 0
+    return dist[dest]
